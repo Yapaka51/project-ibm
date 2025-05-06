fbd12aca79ce1014788872439fb32498e9fc9985
Yapaka51 <yapaka.sa@mail.wu.ac.th>
2025-05-06 15:27:56 +0700

    Add build output to docs

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/project-ibm/",
   plugins: [react()],
+  // base: "/project-ibm/",
+  build: {
+    outDir: "docs",
+  },
 });
